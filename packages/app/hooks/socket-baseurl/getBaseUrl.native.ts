7b0815eb0e516a2ea1c2f9afbd24b0ce732d2e05
NgocDung (Co Phuong Hang) <Dungheongunhubo>
2026-05-15 12:03:54 +0700

    implement 1:1 and group call with grid pagination

--- packages/app/hooks/socket-baseurl/getBaseUrl.native.ts
+++ packages/app/hooks/socket-baseurl/getBaseUrl.native.ts
@@ -1,7 +1,8 @@
 import Constants from 'expo-constants'
 
 export const getBaseUrl = () => {
-  const hostUri = Constants.expoConfig?.hostUri
-  const ip = hostUri ? hostUri.split(':').shift() : '192.168.1.xxx'
-  return `http://${ip}:8085`
+  // const hostUri = Constants.expoConfig?.hostUri
+  // const ip = hostUri ? hostUri.split(':').shift() : '192.168.1.xxx'
+
+  return `http://13.250.21.11:8085`
 }
